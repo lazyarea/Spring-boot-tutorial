5464689623cf41866ced857813c3632a2305c013
Your Name <taro.yamada@example.com>
2016-03-18 19:59:27 +0900

    first commit

---
+++ src/main/java/com/lazyarea/springboot/RootController.java
@@ -0,0 +1,42 @@
+package com.lazyarea.springboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+	String[] names = {"lazyarea","taro","hanako"};
+	String[] mails = {"k4zuhisa@gmail.com","kazuhisa@lazyarea.com"};
+	
+	@RequestMapping("/")
+	public String index(){
+		  return "Hello Spring-boot. ";
+	}
+	
+	@RequestMapping("/{id}")
+	public DataObject index(@PathVariable int id){
+//		return "Hello Spring-boot. " + names[id] + ", " + mails[id];
+		return new DataObject(id,names[id],mails[id]);
+	}
+}
+
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
+	
+	public DataObject(int id, String name, String value){
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+	public int getId() { return id; }
+	public void setId(int id){ this.id = id;}
+	public String getName(){ return name; }
+	public void setName(String name){  this.name = name; }
+	public String getValue(){ return value; }
+	public void setValue(String value){  this.name = value; }
+}
---
+++ src/main/java/com/lazyarea/springboot/StartSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.lazyarea.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringAppApplication.class, args);
+		//System.out.println("hello world.");
+	}
+}
