66f0dc8195d7c84e1dfda675540824bc5f474135
Your Name <taro.yamada@example.com>
2016-03-20 02:35:07 +0900

    3-18

--- src/main/java/com/lazyarea/springboot/RootController.java
+++ src/main/java/com/lazyarea/springboot/RootController.java
@@ -19,9 +19,28 @@ public class RootController {
 	}
 
 	@RequestMapping(value="/", method=RequestMethod.POST)
-	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav){
-		mav.addObject("msg", "こんにちは" + str + "さん。");
-		mav.addObject("value",str);
+	public ModelAndView send(
+			@RequestParam(value="check1", required=false)boolean check1,
+			@RequestParam(value="radio1", required=false)String radio1,
+			@RequestParam(value="select1", required=false)String select1,
+			@RequestParam(value="select2", required=false)String[] select2,
+			ModelAndView mav){
+		String res = "";
+		try {
+			res = "check:" + check1 + 
+					" radio:" + radio1 + 
+					" select1:" + select1 +
+					"\nselect2:";
+		}catch(NullPointerException e){}
+		try{
+			res += select2[0];
+			for(int i=1; i< select2.length;i++){
+				res += ", " + select2[i];
+			}
+		}catch(NullPointerException e){
+				res += "null";
+		}
+		mav.addObject("msg", res);
 		mav.setViewName("index");
 		return mav;
 	}
