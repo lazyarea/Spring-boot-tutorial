62f301a477d0e74bb2eeba608ed9ff061362e6d8
Your Name <taro.yamada@example.com>
2016-03-19 23:32:43 +0900

    3-14

--- src/main/java/com/lazyarea/springboot/RootController.java
+++ src/main/java/com/lazyarea/springboot/RootController.java
@@ -1,16 +1,36 @@
 package com.lazyarea.springboot;
 
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RootController {
 	
-	@RequestMapping("/")
-	public String index(){
-		  return "index";
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav){
+		mav.setViewName("index");
+		mav.addObject("msg", "お名前を書いて送信してください。");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav){
+		mav.addObject("msg", "こんにちは" + str + "さん。");
+		mav.addObject("value",str);
+		mav.setViewName("index");
+		return mav;
 	}
 	
+	@RequestMapping("/{num}")
+	public ModelAndView index(@PathVariable int num, ModelAndView mav){
+		mav.addObject("msg","total: " + num);
+		mav.setViewName("index");
+		return mav;
+	}
+
 }
